46fb01682c939f8a5dff5025bc5ca52f637aa571
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-09-22 19:32:23 +0200

    use strict

--- 03-use-strict/use-strict.js
+++ 03-use-strict/use-strict.js
@@ -1,6 +1,14 @@
-'use strict';
+// 'use strict';
 
 var x = 010;
+console.log('x: ', x);
+
+// 0x65 en hexa means "5" in ASCII table.
+var y = "\065";
+console.log('y: ', y);
+
+var eval = 2.71;
+console.log('eval: ', eval);
 
 // need var
 pi = 3.14;
