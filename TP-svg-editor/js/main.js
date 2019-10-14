a702ff803f67dbe30b174faff7d322f9a8cbcd6d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-14 20:19:17 +0200

    ok

--- TP-svg-editor/js/main.js
+++ TP-svg-editor/js/main.js
@@ -1,14 +1,8 @@
 import { DrawingBoard } from './DrawingBoard.js';
 import { Line } from './widget/Line.js';
 
-
-
-
 const drawingBoard = new DrawingBoard('.board');
+const addLine = () => drawingBoard.prepareForInsert(new Line(drawingBoard));
 
-window.addLine = function () {
-    console.log('addLine', arguments, this);
-    drawingBoard.prepareForInsert(new Line(drawingBoard));
-}
 document.querySelector('button.addLine').addEventListener('click', addLine);
 document.querySelector('button.clean').addEventListener('click', drawingBoard.clean.bind(drawingBoard));
