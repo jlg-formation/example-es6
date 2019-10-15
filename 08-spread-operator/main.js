c5a6e382b458ac9e6e006fe944ef523ed75a8b69
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-15 19:20:25 +0200

    spread operator

---
+++ 08-spread-operator/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+/**
+ * Sum multiplied by a
+ *
+ * @param {*} a
+ * @param {*} others
+ * @returns a * (sum of others)
+ */
+function print(...args) {
+    console.log('printing:', ...args);
+}
+
+print('Hello', 'World');
+
