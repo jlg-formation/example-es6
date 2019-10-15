a17c0157ec2959147fac7e53a5ec9271dfa72fc2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-15 20:31:41 +0200

    format

--- TP-svg-editor/js/DrawingBoard.js
+++ TP-svg-editor/js/DrawingBoard.js
@@ -2,102 +2,102 @@ import { SVGUtils } from "./misc/svg-utils.js";
 import { EditionPoint } from "./EditionPoint.js";
 
 export const MODE = Object.freeze({
-    DEFAULT: 'default',
-    WIDGET_INSERT: 'widget-insert',
-    WIDGET_SELECTED: 'widget-selected',
-    WIDGET_EDITING: 'widget-editing',
+  DEFAULT: 'default',
+  WIDGET_INSERT: 'widget-insert',
+  WIDGET_SELECTED: 'widget-selected',
+  WIDGET_EDITING: 'widget-editing',
 });
 
 export class DrawingBoard {
 
-    constructor(selector) {
-        this.elt = document.querySelector(selector);
-        this.elt.classList.add('drawing-board');
-        this.elt.innerHTML = '<svg></svg><div class="mode"></div>';
-        this.svg = this.elt.querySelector('svg');
-        this.content = SVGUtils.addGroup(this.svg, 'content');
-        this.selectable = SVGUtils.addGroup(this.svg, 'selectable');
-        this.edition = SVGUtils.addGroup(this.svg, 'edition');
-        this.setMode(MODE.DEFAULT);
-        this.svg.addEventListener('click', this.onClick.bind(this));
-        this.widget = null;
-    }
+  constructor(selector) {
+    this.elt = document.querySelector(selector);
+    this.elt.classList.add('drawing-board');
+    this.elt.innerHTML = '<svg></svg><div class="mode"></div>';
+    this.svg = this.elt.querySelector('svg');
+    this.content = SVGUtils.addGroup(this.svg, 'content');
+    this.selectable = SVGUtils.addGroup(this.svg, 'selectable');
+    this.edition = SVGUtils.addGroup(this.svg, 'edition');
+    this.setMode(MODE.DEFAULT);
+    this.svg.addEventListener('click', this.onClick.bind(this));
+    this.widget = null;
+  }
 
-    setMode(mode) {
-        this.mode = mode;
-        for (const value of Object.values(MODE)) {
-            console.log('value: ', value);
-            this.elt.classList.remove(value);
-        }
-        this.elt.classList.add(mode);
-        this.elt.querySelector('.mode').innerHTML = this.mode;
+  setMode(mode) {
+    this.mode = mode;
+    for (const value of Object.values(MODE)) {
+      console.log('value: ', value);
+      this.elt.classList.remove(value);
     }
+    this.elt.classList.add(mode);
+    this.elt.querySelector('.mode').innerHTML = this.mode;
+  }
 
-    prepareForInsert(widget) {
-        console.log('widget: ', widget);
-        this.setMode(MODE.WIDGET_INSERT);
-        console.log('this', this);
-        this.widget = widget;
-    }
+  prepareForInsert(widget) {
+    console.log('widget: ', widget);
+    this.setMode(MODE.WIDGET_INSERT);
+    console.log('this', this);
+    this.widget = widget;
+  }
 
-    select(widget) {
-        this.setMode(MODE.WIDGET_SELECTED);
-        this.widget = widget;
-        this.widget.select();
-    }
+  select(widget) {
+    this.setMode(MODE.WIDGET_SELECTED);
+    this.widget = widget;
+    this.widget.select();
+  }
 
-    onClick(event) {
-        console.log('onClick', this, arguments);
-        if (this.mode === MODE.WIDGET_INSERT) {
-            this.widget.depose(event);
-            this.setMode(MODE.WIDGET_SELECTED);
-            this.widget.select();
-            return;
-        }
-        if (this.mode === MODE.WIDGET_SELECTED) {
-            console.log('going to default');
-            this.widget.unselect();
-            this.setMode(MODE.DEFAULT);
-            return;
-        }
-        if (this.mode === MODE.WIDGET_EDITING) {
-            this.setMode(MODE.WIDGET_SELECTED);
-            return;
-        }
+  onClick(event) {
+    console.log('onClick', this, arguments);
+    if (this.mode === MODE.WIDGET_INSERT) {
+      this.widget.depose(event);
+      this.setMode(MODE.WIDGET_SELECTED);
+      this.widget.select();
+      return;
     }
+    if (this.mode === MODE.WIDGET_SELECTED) {
+      console.log('going to default');
+      this.widget.unselect();
+      this.setMode(MODE.DEFAULT);
+      return;
+    }
+    if (this.mode === MODE.WIDGET_EDITING) {
+      this.setMode(MODE.WIDGET_SELECTED);
+      return;
+    }
+  }
 
-    append(elt) {
-        this.content.appendChild(elt);
-    }
+  append(elt) {
+    this.content.appendChild(elt);
+  }
 
-    addEditionPoint(label, x, y, onClickFn) {
-        console.log('add edition point');
-        const circle = (new EditionPoint(x, y, label, onClickFn)).getGroup();
-        this.edition.appendChild(circle);
-    }
+  addEditionPoint(label, x, y, onClickFn) {
+    console.log('add edition point');
+    const circle = (new EditionPoint(x, y, label, onClickFn)).getGroup();
+    this.edition.appendChild(circle);
+  }
 
-    getEditionPointElt(label) {
-        const result = this.edition.querySelector(`g.${label} circle`);
-        console.log('result: ', result);
-        return result;
-    }
+  getEditionPointElt(label) {
+    const result = this.edition.querySelector(`g.${label} circle`);
+    console.log('result: ', result);
+    return result;
+  }
 
-    clean() {
-        SVGUtils.removeAllChildren(this.content);
-        SVGUtils.removeAllChildren(this.selectable);
-        SVGUtils.removeAllChildren(this.edition);
-    }
+  clean() {
+    SVGUtils.removeAllChildren(this.content);
+    SVGUtils.removeAllChildren(this.selectable);
+    SVGUtils.removeAllChildren(this.edition);
+  }
 
-    removeAllEditionPoint() {
-        SVGUtils.removeAllChildren(this.edition);
-    }
+  removeAllEditionPoint() {
+    SVGUtils.removeAllChildren(this.edition);
+  }
 
-    selectFromClickEvent(widget) {
-        return event => {
-            event.stopPropagation();
-            console.log('select');
-            this.select(widget);
-        }
+  selectFromClickEvent(widget) {
+    return event => {
+      event.stopPropagation();
+      console.log('select');
+      this.select(widget);
     }
+  }
 
 }
--- TP-svg-editor/js/EditionPoint.js
+++ TP-svg-editor/js/EditionPoint.js
@@ -1,32 +1,32 @@
 export class EditionPoint {
-    constructor(x, y, label, onMouseDownFn) {
-        this.group = document.createElementNS("http://www.w3.org/2000/svg", 'g');
-        this.group.setAttribute('class', label);
-    
-        const circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
-        circle.setAttribute('r', 3);
-        circle.setAttribute('cx', x);
-        circle.setAttribute('cy', y);
-        circle.setAttribute('fill', 'white');
-        circle.setAttribute('stroke', 'black');
-        this.group.appendChild(circle);
+  constructor(x, y, label, onMouseDownFn) {
+    this.group = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+    this.group.setAttribute('class', label);
 
-        const clickableArea = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
-        clickableArea.setAttribute('r', 10);
-        clickableArea.setAttribute('cx', x);
-        clickableArea.setAttribute('cy', y);
-        clickableArea.setAttribute('fill', 'transparent');
-        clickableArea.setAttribute('stroke', 'transparent');
-        clickableArea.setAttribute('class', 'clickable');
-        clickableArea.addEventListener('mousedown', event => {
-            event.stopPropagation();
-            onMouseDownFn(event);
-        });
-        this.group.appendChild(clickableArea);
-        
-    }
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    circle.setAttribute('r', 3);
+    circle.setAttribute('cx', x);
+    circle.setAttribute('cy', y);
+    circle.setAttribute('fill', 'white');
+    circle.setAttribute('stroke', 'black');
+    this.group.appendChild(circle);
 
-    getGroup() {
-        return this.group;
-    }
+    const clickableArea = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    clickableArea.setAttribute('r', 10);
+    clickableArea.setAttribute('cx', x);
+    clickableArea.setAttribute('cy', y);
+    clickableArea.setAttribute('fill', 'transparent');
+    clickableArea.setAttribute('stroke', 'transparent');
+    clickableArea.setAttribute('class', 'clickable');
+    clickableArea.addEventListener('mousedown', event => {
+      event.stopPropagation();
+      onMouseDownFn(event);
+    });
+    this.group.appendChild(clickableArea);
+
+  }
+
+  getGroup() {
+    return this.group;
+  }
 }
