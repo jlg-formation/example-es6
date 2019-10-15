560ab0ae03d4f0c61c59b83aceff8a8682d85114
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-15 20:32:05 +0200

    format

--- TP-svg-editor/js/misc/svg-utils.js
+++ TP-svg-editor/js/misc/svg-utils.js
@@ -1,22 +1,22 @@
 export class SVGUtils {
-    static getCoordinates(event) {
-        const e = event.target;
-        const dim = e.getBoundingClientRect();
-        const x = event.clientX - dim.left;
-        const y = event.clientY - dim.top;
-        return { x, y };
-    }
+  static getCoordinates(event) {
+    const e = event.target;
+    const dim = e.getBoundingClientRect();
+    const x = event.clientX - dim.left;
+    const y = event.clientY - dim.top;
+    return { x, y };
+  }
 
-    static removeAllChildren(group) {
-        while (group.firstChild) {
-            group.removeChild(group.firstChild);
-        }
+  static removeAllChildren(group) {
+    while (group.firstChild) {
+      group.removeChild(group.firstChild);
     }
+  }
 
-    static addGroup(parent, name) {
-        const group = document.createElementNS("http://www.w3.org/2000/svg", 'g');
-        group.setAttribute('class', name);
-        parent.appendChild(group);
-        return group;
-    }
-} 
+  static addGroup(parent, name) {
+    const group = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+    group.setAttribute('class', name);
+    parent.appendChild(group);
+    return group;
+  }
+}
