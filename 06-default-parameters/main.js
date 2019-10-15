665a8abd8528d0553015432eb4fe90b64bc2da89
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-15 19:09:02 +0200

    default params

--- 06-default-parameters/main.js
+++ 06-default-parameters/main.js
@@ -6,3 +6,8 @@ function increment(a, step = 1) {
 console.log('3 + 1 =', increment(3));
 console.log('3 + 2 =', increment(3, 2));
 
+const decrement = (a, step = 1) => a - step;
+
+console.log('3 - 1 =', decrement(3));
+console.log('3 - 2 =', decrement(3, 2));
+
