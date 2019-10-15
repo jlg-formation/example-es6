c7fa3d2566b1229d04792ba8bdfa7516bb16f581
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-15 20:30:08 +0200

    format

--- 01-variables/let-const.js
+++ 01-variables/let-const.js
@@ -1,38 +1,37 @@
 try {
-    const a = 45;
-    console.log('a: ', a);
+  const a = 45;
+  console.log('a: ', a);
 } catch (e) {
-    console.log('e: ', e.message);
+  console.log('e: ', e.message);
 }
 
 const b = 3;
 try {
-    b = 4;
-} catch(e) {
-    console.log('e: ', e.message);
+  b = 4;
+} catch (e) {
+  console.log('e: ', e.message);
 }
 
 try {
-    c = 4;
-    const c = 5;
+  c = 4;
+  const c = 5;
 } catch (e) {
-    console.log('e: ', e);
+  console.log('e: ', e);
 }
 
 try {
-    const d = {
-        toto: 'titi'
-    };
-    d.toto = 45;
-    console.log('d: ', d);
+  const d = {
+    toto: 'titi'
+  };
+  d.toto = 45;
+  console.log('d: ', d);
 } catch (e) {
-    console.log('e: ', e.message);    
+  console.log('e: ', e.message);
 }
 
 let e = 3;
 e = 4;
 console.log('e: ', e);
-
 
 // try this just to see...
 const a = 456;
