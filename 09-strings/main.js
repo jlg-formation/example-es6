e1d8ee8c9b51c12e37850ae7a1cd8e258605a8ec
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-20 11:00:48 +0200

    strings

---
+++ 09-strings/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const a = 'J\'ai envie de programmer';
+const b = "You will say \"Hello\"";
+
+// template string
+const verb = 'programmer';
+const c = `J'ai envie
+de ${verb}`;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+function pluralize(strings, ...expressionArray) {
+  const array = [strings[0]];
+  for (let i = 1; i < strings.length; i++) {
+    const expr = expressionArray[i - 1];
+    const string = strings[i];
+    if (typeof expr !== 'number') {
+      array.push(expr, string);
+      break;
+    }
+    if (expressionArray >= 2) {
+      // console.log('plural case');
+      const str = string.replace('(s)', 's');
+      array.push(expr, str);
+      break;
+    }
+    // console.log('singular case');
+    const str = string.replace('(s)', '');
+    array.push(expr, str);
+  }
+  return array.join('');
+}
+
+console.log(pluralize`Je veux ${2} mouton(s)`);
+console.log(pluralize`Je veux ${1} mouton(s)`);
