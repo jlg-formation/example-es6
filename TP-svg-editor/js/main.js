253b972e22aad671d64916e47f69d13f8e5aa397
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-09 15:59:52 +0100

    remove tp

--- TP-svg-editor/js/main.js
+++
@@ -1,8 +0,0 @@
-import { DrawingBoard } from './DrawingBoard.js';
-import { Line } from './widget/Line.js';
-
-const drawingBoard = new DrawingBoard('.board');
-const addLine = () => drawingBoard.prepareForInsert(new Line(drawingBoard));
-
-document.querySelector('button.addLine').addEventListener('click', addLine);
-document.querySelector('button.clean').addEventListener('click', drawingBoard.clean.bind(drawingBoard));
